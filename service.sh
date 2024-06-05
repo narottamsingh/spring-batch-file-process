b9f8998f103bbe74eb5792cf501c7ee0791709f0
narottamsingh <n.kumar9@yahoo.com>
2024-06-05 09:30:20 +0530

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-service_name="jamesservice"
-service_script="run.sh"
+service_name="james"
+service_script="james.sh"
+check_interval=10 # interval in seconds
 
 start_service() {
-    echo "Starting ${service_name}..."
-    nohup ./${service_script} >/dev/null 2>&1 &
+    pids=$(ps aux | grep -v grep | grep "${service_script}" | awk '{print $2}')
+    if [ -z "$pids" ]; then
+	echo "Starting ${service_name}..."
+        nohup ./${service_script} >/dev/null 2>&1 &
+        echo "James service started successfully"
+    else
+    	echo "Service already running at this pid $pids"
+    fi     
 }
 
 stop_service() {
-    echo "Stopping ${service_name}..."
-    sudo systemctl stop ${service_name}
+    pids=$(ps aux | grep -v grep | grep "${service_script}" | awk '{print $2}')
+    if [ -z "$pids" ]; then
+        echo "${service_name} is not running."
+    else
+        echo "${service_name} is running with PIDs: $pids"
+        echo "Stopping ${service_name}..."
+        for pid in $pids; do
+            kill -9 $pid
+            echo "james service successfully stopped for pid $pid"
+        done
+    fi
 }
 
 status_service() {
-    sudo systemctl status ${service_name}
-}
-
-check_service() {
     if ps aux | grep -v grep | grep -q "${service_script}"; then
         echo "${service_name} is running."
     else
@@ -25,13 +37,30 @@ check_service() {
     fi
 }
 
+check_service() {
+    if ps aux | grep -v grep | grep -q "${service_script}"; then
+        echo "${service_name} is running."
+        return 0
+    else
+        echo "${service_name} is not running."
+        return 1
+    fi
+}
+
 get_pid() {
-    pid=$(ps aux | grep -v grep | grep "${service_script}" | awk '{print $2}')
-    if [ -z "$pid" ]; then
+    pids=$(ps aux | grep -v grep | grep "${service_script}" | awk '{print $2}')
+    if [ -z "$pids" ]; then
         echo "${service_name} is not running."
     else
-        echo "${service_name} is running with PID $pid."
+        echo "${service_name} is running with PIDs: $pids"
     fi
+}
+
+continuous_check() {
+    while true; do
+        check_service
+        sleep $check_interval
+    done
 }
 
 case "$1" in
@@ -54,8 +83,11 @@ case "$1" in
     pid)
         get_pid
         ;;
+    loop)
+        continuous_check
+        ;;
     *)
-        echo "Usage: $0 {start|stop|restart|status|check|pid}"
+        echo "Usage: $0 {start|stop|restart|status|check|pid|loop}"
         exit 1
         ;;
 esac
